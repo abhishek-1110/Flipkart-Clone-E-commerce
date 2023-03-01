2717723744852a1f59b3b0e2bc3542ba00ac2a91
Abhishek <abhishkbhi@gmail.com>
2023-03-01 22:07:07 +0530

    added electronicsProduct constant

---
+++ client/src/redux/constants/electronicsProductConstant.js
@@ -0,0 +1,7 @@
+export const GET_ELECTRONICS_PRODUCTS_SUCCESS = "getElectronicsProductsSuccess";
+export const GET_ELECTRONICS_PRODUCTS_FAIL = "getElectronicsProductsFail";
+
+export const GET_ELECTRONICS_PRODUCTS_DETAILS_REQUEST = "getElectronicsProductsDetailsRequest";
+export const GET_ELECTRONICS_PRODUCTS_DETAILS_SUCCESS = "getElectronicsProductsDetailsSuccess";
+export const GET_ELECTRONICS_PRODUCTS_DETAILS_FAIL = "getElectronicsProductsDetailsFail";
+export const GET_ELECTRONICS_PRODUCTS_DETAILS_RESET = "getElectronicsProductsDetailsReset";
