6fbd28ec0857fa1cc86e16d1d76b8263beb60934
Abhishek <abhishkbhi@gmail.com>
2023-03-18 18:08:03 +0530

    removed username

--- server/model/user-schema.js
+++ server/model/user-schema.js
@@ -1,46 +1,38 @@
-
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-    firstname: {
-        type: String,
-        required: true,
-        trim: true,
-        min: 5,
-        max: 20
-    },
-    lastname: {
-        type: String,
-        required: true,
-        trim: true,
-        min: 5,
-        max: 20
-    },
-    username: {
-        type: String,
-        required: true,
-        trim: true,
-        unique: true,
-        index: true,
-        lowercase: true
-    },
-    email: {
-        type: String,
-        required: true,
-        trim: true,
-        unique: true,
-        lowercase: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    phone: {
-        type: String,
-        required: true
-    }
+  firstname: {
+    type: String,
+    required: true,
+    trim: true,
+    min: 5,
+    max: 20,
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    unique: true,
+    lowercase: true,
+    index: true,
+
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  confirmpassword: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+    unique: true,
+    index: true,
+  },
 });
 // user is database name in mongodb
-const user = mongoose.model('user', userSchema);
+const user = mongoose.model("user", userSchema);
 
 export default user;
