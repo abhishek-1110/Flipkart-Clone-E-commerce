7ee341b856db5b3c9dda3f64af03e5295ed3d0ef
Abhishek <abhishkbhi@gmail.com>
2023-03-18 18:10:59 +0530

    refactored

--- client/src/components/login/LoginDialog.jsx
+++ client/src/components/login/LoginDialog.jsx
@@ -7,10 +7,9 @@ import {
   styled,
 } from "@mui/material";
 
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import { authenticateSignup, authenticateLogin } from "../../service/api";
 import { DataContext } from "../../context/DataProvider";
-
 
 const Component = styled(Box)`
   height: 70vh;
@@ -95,15 +94,14 @@ const accountInitialValue = {
 
 const signUpInitialValues = {
   firstname: "",
-  lastname: "",
-  username: "",
   email: "",
   password: "",
+  confirmpassword: "",
   phone: "",
 };
 
 const loginInitialValues = {
-  username: "",
+  email: "",
   password: "",
 };
 
@@ -129,6 +127,9 @@ const LoginDialog = ({ open, setOpen }) => {
   // states
   const [error, showError] = useState(false);
 
+  // for phone
+  const [phoneError, setphoneError] = useState(false);
+
   const handleClose = () => {
     setOpen(false);
     showError(false);
@@ -136,39 +137,94 @@ const LoginDialog = ({ open, setOpen }) => {
   };
 
   const toggleSignup = () => {
+    showError(false);
     toggleAccount(accountInitialValue.signup);
   };
 
   const toggleLogin = () => {
+    showError(false);
     toggleAccount(accountInitialValue.login);
-  }
+  };
 
   const onInputChange = (e) => {
     setSignup({ ...signup, [e.target.name]: e.target.value });
+    setAlreadyExists("");
   };
 
+  let passWordIndicator = false;
+
+  const [alreadyExists, setAlreadyExists] = useState(false);
+
   const signupUser = async () => {
-    let response = await authenticateSignup(signup);
-    handleClose();
-    setAccount(signup.username);
-    if (!response) return;
-    else {
+    console.log(signup);
+    // to show error when password doesn't matches
+
+    try {
+      // console.log(signup.firstname);
+      if (
+        signup.firstname.length === 0 ||
+        signup.phone.length === 0 ||
+        signup.password.length === 0 ||
+        signup.confirmpassword.length === 0
+      ) {
+        showError(true);
+        return;
+      }
+
+
+      if (signup.phone.length < 10 || signup.phone.length > 10) {
+         setphoneError(true);
+         return;
+      }
+
+      let response = await authenticateSignup(signup);
+
+      if (response === 401 || response === 500) {
+        // console.log("Already exists");
+        setAlreadyExists(true);
+        return;
+      }
+
+      //console.log(response.data);
+      handleClose();
+      setAccount(signup.firstname);
+      localStorage.setItem("loggedinUser", signup.firstname);
+      if (!response) return;
+      else {
+      }
+    } catch (error) {
+      console.log("Something went wrong", error);
     }
   };
 
   const onValueChange = (e) => {
+    showError(false);
+    setphoneError(false);
     setLogin({ ...login, [e.target.name]: e.target.value });
   };
 
   const loginUser = async () => {
     let response = await authenticateLogin(login);
-    if (!response) showError(true);
-    else {
+    // console.log("Front end", response.data);
+    if (response === 401) {
+      showError(true);
+      return;
+    } else {
       showError(false);
       handleClose();
-      setAccount(login.username);
+      setAccount(response.data);
+      localStorage.setItem("loggedinUser", response.data);
     }
   };
+
+  useEffect(() => {
+    return () => {
+      if (localStorage.getItem("loggedinUser")) {
+        setAccount(localStorage.getItem("loggedinUser"));
+        // console.log(setAccount);
+      }
+    };
+  }, []);
 
   return (
     // unsetting width of dialog
@@ -190,9 +246,10 @@ const LoginDialog = ({ open, setOpen }) => {
             <Wrapper style={{ paddingTop: "0" }}>
               <TextField
                 onChange={(e) => onValueChange(e)}
-                name="username"
+                type="email"
+                name="email"
                 id="standard-basic"
-                label="Enter username"
+                label="Enter email"
                 variant="standard"
               />
               {error && <Error>Please enter valid Credentials.</Error>}
@@ -202,6 +259,7 @@ const LoginDialog = ({ open, setOpen }) => {
                 id="standard-basic"
                 label="Enter Password"
                 variant="standard"
+                type="password"
               />
               <Text>
                 By continuing, you agree to Flipkart's Terms of Use and Privacy
@@ -218,46 +276,64 @@ const LoginDialog = ({ open, setOpen }) => {
             <Wrapper style={{ paddingTop: "0" }}>
               <TextField
                 id="standard-basic"
-                label="Enter First Name"
+                label="Enter Name"
                 variant="standard"
                 onChange={(e) => onInputChange(e)}
                 name="firstname"
+                required
               />
-              <TextField
-                id="standard-basic"
-                label="Enter Last Name"
-                variant="standard"
-                onChange={(e) => onInputChange(e)}
-                name="lastname"
-              />
-              <TextField
-                id="standard-basic"
-                label="Enter Username"
-                variant="standard"
-                onChange={(e) => onInputChange(e)}
-                name="username"
-              />
+
               <TextField
                 id="standard-basic"
                 label="Enter Email"
                 variant="standard"
                 onChange={(e) => onInputChange(e)}
                 name="email"
+                type="email"
+                required
               />
-              <TextField
-                id="standard-basic"
-                label="Enter Password"
-                variant="standard"
-                onChange={(e) => onInputChange(e)}
-                name="password"
-              />
+
               <TextField
                 id="standard-basic"
                 label="Enter Phone"
                 variant="standard"
                 onChange={(e) => onInputChange(e)}
                 name="phone"
+                type="number"
+                required
               />
+              {phoneError && (
+                <Error>Please enter valid 10 digit phone number.</Error>
+              )}
+
+              <TextField
+                id="standard-basic"
+                label="Enter Password"
+                variant="standard"
+                onChange={(e) => onInputChange(e)}
+                name="password"
+                type="password"
+                autoComplete="off"
+                required
+              />
+
+              <TextField
+                id="standard-basic"
+                label="Re-enter Password"
+                variant="standard"
+                type="password"
+                onChange={(e) => onInputChange(e)}
+                name="confirmpassword"
+                required
+              />
+
+              {passWordIndicator && error && (
+                <Error>Password doesn't match.</Error>
+              )}
+
+              {alreadyExists && <Error>Email or Phone Number already exists.</Error>}
+              {error && <Error>One or more field(s) are empty.</Error>}
+
               <LoginButton onClick={() => signupUser()}>Continue</LoginButton>
               <CreateAccount onClick={() => toggleLogin()}>
                 Already have an account? Login
